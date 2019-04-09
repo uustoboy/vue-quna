0b4809d708ada1f54bb79e250a9933b5596ca192
uustoboy <tianqi_email@163.com>
2019-04-10 00:03:12 +0800

    add

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router/'
 import store from './store'
+
+import './assets/styles/reset.css'
+import "./assets/styles/border.css"
+
+import fastClick from 'fastclick'
+
+fastClick.attach(document.body);
 
 Vue.config.productionTip = false
 
