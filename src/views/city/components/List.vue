747e9b269b7acd52f4fb6c163ade90abf2e98405
uustoboy <tianqi_email@163.com>
2019-04-11 19:11:05 +0800

    add

---
+++ src/views/city/components/List.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="">
+    list
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityList',
+  components: {
+  },
+  data (){
+    return {
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+</style>
+
