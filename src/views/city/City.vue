7f44210407c2f5ed0dce45e24df8b7986194561a
uustoboy <tianqi_email@163.com>
2019-04-11 13:27:37 +0800

    city router

---
+++ src/views/city/City.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <city-header/>
+  </div>
+</template>
+
+<script>
+import CityHeader from './components/Header'
+export default {
+  name: 'City',
+  components: {
+    CityHeader
+  },
+  data (){
+    return {
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+</style>
+
--- src/views/home/components/Header.vue
+++ src/views/home/components/Header.vue
@@ -6,10 +6,12 @@
     <div class="header-input">
       <span class="iconfont">&#xe632;</span>
       输入城市/景点/游玩主体</div>
-    <div class="header-right">
-      {{this.city}}
-      <span class="iconfont arrow-icon">&#xe6aa;</span>
-    </div>
+    <router-link to="/city">
+      <div class="header-right">
+        {{this.city}}
+        <span class="iconfont arrow-icon">&#xe6aa;</span>
+      </div>
+    </router-link>
   </div>
 </template>
 
@@ -31,7 +33,7 @@ export default {
   .header
     display flex
     height .86rem
-    line-height .86rem
+    line-height $headerHeight
     background $bgColor
     color #fff
     .header-left
@@ -53,6 +55,7 @@ export default {
       width 1.24rem
       float right
       text-align center
+      color:#fff;
       .arrow-icon
         font-size .24rem
         margin-left -.04rem
