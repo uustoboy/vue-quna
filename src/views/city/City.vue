da9fecd17247e9f22ff317265c3d78a0bb8492e3
uustoboy <tianqi_email@163.com>
2019-04-13 01:06:08 +0800

    add

--- src/views/city/City.vue
+++ src/views/city/City.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <city-header/>
-    <city-search/>
+    <city-search :cities="cities"/>
     <city-list :cities="cities" :hot="hotCities" :letter="letter"/>
     <city-alphabet :cities="cities" @change="handleLetterClick"/>
   </div>
