8b1f0e14bd1b69d665b23ccd0ec0e24bc4c02afc
uustoboy <tianqi_email@163.com>
2019-04-15 23:24:24 +0800

    add

---
+++ src/views/detail/components/List.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailList',
+  props:{
+    list: Array
+  },
+  components: {
+
+  },
+  data () {
+    return {
+
+    }
+  },
+  methods:{
+
+  },
+  activated(){
+  },
+  deactivvated(){
+  },
+  mounted () {
+  },
+  computed: {
+
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  //1rem = html font-size = 50px;
+  //86/100;
+
+</style>
