fdcc3f0a829d3bbb0e02c5443b79c731f3eed243
uustoboy <tianqi_email@163.com>
2019-04-16 11:32:38 +0800

    add

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -25,7 +25,20 @@ export default {
     return {
       list:[
         {
-          title:'成人票'
+          title:'成人票',
+          children:[
+            {
+              title:'成人三馆联票',
+              children:[
+                {
+                  title:'成人三馆联票-某一连锁店'
+                }
+              ]
+            },
+            {
+              title:'成人五馆联票'
+            }
+          ]
         },
 
         {
--- src/views/detail/components/List.vue
+++ src/views/detail/components/List.vue
@@ -1,6 +1,14 @@
 <template>
     <div>
-
+      <div class="item" v-for="(item,index) of list" :key="index">
+        <div class="item-title border-bottom">
+          <span class="item-title-icon"></span>
+          {{item.title}}
+        </div>
+        <div v-if="item.children" class="item-children">
+          <detail-list :list="item.children"></detail-list>
+        </div>
+      </div>
     </div>
 </template>
 
@@ -35,5 +43,21 @@ export default {
 <style lang="stylus" scoped>
   //1rem = html font-size = 50px;
   //86/100;
+.item-title-icon
+  position relative
+  left .06rem
+  top 0.09rem
+  display inline-block
+  width .36rem
+  height .36rem
+  background url(http://s.qunarzz.com/piao/image/touch/sight/detail.png) 0 -.45rem no-repeat
+  margin-right .1rem
+  background-size .4rem 3rem
+.item-title
+  line-height .8rem
+  font-size .32rem
+  padding 0 .2rem
+.item-children
+  padding 0 .1rem
 
 </style>
